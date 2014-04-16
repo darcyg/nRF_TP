8e1404bf2cb473b1ec979ec9a94b337992ccaa4e
Gabor Beres <gaborr.b@gmail.com>
2014-04-16 22:28:20 +0200

    Ping folyamat kommentelése, TODOk felvétele hozzá

--- nRFTP/nRFTransportProtocol.cpp
+++ nRFTP/nRFTransportProtocol.cpp
@@ -59,27 +59,41 @@ namespace nRFTP {
       }
 
       void nRFTransportProtocol::run(void){
+        // a waitingForPingResponse ha nem nulla, akkor az az idõ van benne, mikor az utolsó ping üzenet küldve lett, ha nulla, akkor nem várunk válaszra
         if (waitingForPingResponse > 0){
+        	// Ide akkor lép be, ha válaszra várunk
+
+        	// A millis a mostani idõ, a waitingForPingResponse a küldési idõ, ha a kettõ között kevesebb idõ telt el, mint a MAX_WAIT_TIME (2000ms), akkor megnézzük, hogy jött-e csomag
             if (millis() - waitingForPingResponse < PingMessage::MAX_WAIT_TIME){
-            	delay(3);
+              // ha ide belépett, akkor még nem telt el annyi idõ, hogy timeout legyen, de vár a válaszra
+
+              // itt nézi, hogy van e csomag
               if (available()){
+            	// van csomag
+
+            	// beolvassa a csomag payloadját a readBuffer-be, és megnézi a típusát
                 ByteBuffer bb(readBuffer);
                 read(bb);
                 readedType = Message::getTypeFromReadBuffer(readBuffer);
 
+                // Ha ping csomag érkezett
                 if (readedType == Message::TYPE_PING){
-#if DEBUG_NRFTP == 1
-                  Header h(bb);
-                  h.printHeader();
-#endif // DEBUG_NRFTP
+
+                  // ping csomag érkezett, értesíti az appot a messageHandleren keresztül, de nem az általános handleMessageve, hanem a pingResponseArroved-del
+                  // TODO: Ez így még nem jó, mert attól hogy ping csomag, lehet hogy egy másik node pingelt minket, ilyenkor nem válasznak kell kezelni, hanem válaszolni rá, továbbítani a csomagokt a protokoll fõ csomagkezelõjébe (ami még nincs kész)
                   messageHandler->pingResponseArrived((uint16_t)(millis() - waitingForPingResponse), currentlyPingingAddress );
-                  //waitingForPingResponse = 0;
-                  //currentlyPingingAddress = 0;
+
+                  // Ezeket nullázza, a waitingForPingResponse-et azért, mert már nem várakozunk, értelemszerûen a currentlyPingingAddress-t is
+                  waitingForPingResponse = 0;
+                  currentlyPingingAddress = 0;
                 } else {
+                  // Jött egy csomag várakozás közben, de nem ping típusú, ezért továbbítjuk az appnak
+                  // TODO: Nem jó így, mert a protokoll csomagkezelõjét így kikerüli, ezért a protokoll fõ csomagkezelõjébe kell továbbítani, ami még nincs meg
                   messageHandler->handleMessage(bb, readedType);
                 }
               }
             } else {
+              // Ide akkor jön, ha mostani idõ és a waitingForPingResponse különbsége nagyobb, mint a MAX_WAIT_TIME. Ekkor az alkalmazásnak küld egy timeoutot (a 9999-es válasz), és abbahagyja a várakozást (nullázás)
               messageHandler->pingResponseArrived(PingMessage::TIMEOUT_VAL, currentlyPingingAddress );
               waitingForPingResponse = 0;
               currentlyPingingAddress = 0;
@@ -101,14 +115,22 @@ namespace nRFTP {
 
           switch (readedType){
             case Message::TYPE_PING:
+              // Jött egy ping üzenet
+
+              // Csak akkor válaszol rá, ha jelenleg nem várakozunk
+              // TODO: Ez így nem jó, akkor is válaszolni kell, ha várakozunk, közbe pedig minket ping requestelnek
+              // TODO: A run metódus elején lévõ pinges részt amennyire lehet ide át kellene rakni, csak a timeout jelzés képessége maradjon meg
               if (waitingForPingResponse == 0){
+            	// Objektum létrehozása, cím cseréje
                 PingMessage pingMessage(bb);
                 uint16_t tmp = pingMessage.header.srcAddress;
                 pingMessage.header.srcAddress = pingMessage.header.destAddress;
                 pingMessage.header.destAddress = tmp;
+
+                // bytebuffer reset a copy elõtt, aztán küldés
+                bb.reset();
                 pingMessage.copyToByteBuffer(bb);
                 sendMessage(bb, pingMessage.header.destAddress);
-                bb.reset();
               }
             break;
 
